39c8fe78e4146a5efc6fdc79c01bc8724e32f043
Md. Mahmudul Hasan Khan <hasan63126@gmail.com>
2025-07-19 11:53:48 +0600

    Temperature Converter

---
+++ TemperatureConverter.py
@@ -0,0 +1,5 @@
+C= float(input("Enter the temperature in Celsius : "))
+F = (C * 9/5) + 32
+K = C + 273.15
+print(f"The temperature in Fahrenheit is: {F:.2f}")
+print(f"The temperature in Kelvin is: {K:.2f}")
