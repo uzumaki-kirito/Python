a911701ca7806398f85cce896560b48446d1dc5c
Md. Mahmudul Hasan Khan <hasan63126@gmail.com>
2025-07-19 14:04:05 +0600

    Modified Simple Dict 2

--- HighestGrade.py
+++ HighestGrade.py
@@ -4,6 +4,8 @@ for i in range(n):
     name = input("Enter the student name: ")
     grade = int(input("Enter the student grade: "))
     Students.append({"Name": name, "Grade": grade})
+highest = Students[0]
 for student in Students:
-    if student["Grade"] > 80:
-        print(student["Name"])
+    if student["Grade"] > highest["Grade"]:
+        highest = student
+print(f"Top student: {highest['Name']}")
