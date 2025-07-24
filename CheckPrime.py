1f694b49ab44da49390a294f18bbc350423792af
Md. Mahmudul Hasan Khan <hasan63126@gmail.com>
2025-07-24 11:40:12 +0600

    Prime function

---
+++ CheckPrime.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+Num = int(input("Enter a number: "))
+if is_prime(Num):
+    print("Prime")
+else:
+    print("Not Prime")
